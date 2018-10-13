f1baca5bb1307c5fce19e2b10c358c13350f2c2c
chimWang <chimwang1203@gmail.com>
2018-10-13 13:42:55 +0800

    chim

--- client/routes/index.js
+++ client/routes/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 ReactDOM.render(
-    <h1>hello world!!!!!!!hahaahah</h1>,
+    <h1>hello world!</h1>,
     document.getElementById('app')
 )
