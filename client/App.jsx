0389d3321994352be771322fc8b91fb0ba71f6bf
chimWang <chimwang1203@gmail.com>
2018-10-19 11:09:58 +0800

    chim

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import Main from './modules/main/Main'
 import './App.less'
 
 const App=()=>{
     return(
-        <Main/>
+        <div className='bg'>great!!!!</div>
     )
 }
 
